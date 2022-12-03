d005e688b0bd6c096b6efebec1c4caaf7a14bbe0
BigStoagie <103386806+BigStoagie@users.noreply.github.com>
2022-12-03 13:22:28 -0500

    Fixed an issue on my end with a naming mismatch between Bullet class and Bullet script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [Header("---- Bullet Settings ----")]
     [SerializeField] int speed;
