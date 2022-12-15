d05fdb993daa048aacf9e5711b46765b31970846
BigStoagie <103386806+BigStoagie@users.noreply.github.com>
2022-12-15 06:59:32 -0500

    fixes

--- Assets/Scripts/explosion.cs
+++ Assets/Scripts/explosion.cs
@@ -64,7 +64,7 @@ public class explosion : MonoBehaviour
             RigBdy.AddExplosionForce(explosionForce, transform.position, explosionRadius);
         }
 
-        if (other.GetComponent<IDamage>() != null)
+        if (other.GetComponent<IDamage>() != null && canDamageEnemy)
         {
             StartCoroutine(giveEnemyDamage(other.GetComponent<IDamage>()));
         }
