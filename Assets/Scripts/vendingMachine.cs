5eeea3c8ff6a5973e326c549bbb9265c91d7bafa
BigStoagie <103386806+BigStoagie@users.noreply.github.com>
2022-12-14 20:56:58 -0500

    Vending Machine

---
+++ Assets/Scripts/vendingMachine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vendingMachine : MonoBehaviour
+{
+    [SerializeField] GameObject collectable;
+    [SerializeField] AudioSource speaker;
+    [SerializeField] AudioClip jingle;
+    [SerializeField] AudioClip failSound;
+    [SerializeField] int cost;
+    [SerializeField] Transform trayPos;
+    [SerializeField] float delay;
+
+    bool canPurchase = true;
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && Input.GetButtonDown("Interact") && gameManager.instance.credits >= cost && canPurchase)
+        {
+            StartCoroutine(dispense());
+
+        }
+    }
+
+    IEnumerator dispense()
+    {
+        canPurchase = false;
+        gameManager.instance.credits -= cost;
+        Instantiate(collectable, trayPos.position, transform.rotation);
+        speaker.PlayOneShot(jingle);
+        yield return new WaitForSeconds(delay);
+        canPurchase = true;
+    }
+}
