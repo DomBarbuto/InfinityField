6ad9cddb6ed35213dfc1f2bf82af2f9357af4563
BigStoagie <103386806+BigStoagie@users.noreply.github.com>
2023-01-09 09:45:20 -0500

    Changed out of bounds to damage collider + level work

---
+++ Assets/Scripts/Environment/damageCollider.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageCollider : MonoBehaviour
+{
+    [SerializeField] int dmg;
+    [SerializeField] float cooldown;
+
+    bool inCooldown;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && !inCooldown)
+        {
+            StartCoroutine(damage());
+        }
+    }
+
+    IEnumerator damage()
+    {
+        inCooldown = true;
+        gameManager.instance.playerController.takeDamage(dmg);
+        yield return new WaitForSeconds(cooldown);
+        inCooldown = false;
+
+    }
+}
