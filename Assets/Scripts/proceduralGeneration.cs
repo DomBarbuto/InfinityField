c70c54f44860c381656eaea1dad84cdb1fad3ec1
Tanner Clark <tannerclark0307@gmail.com>
2023-01-16 13:07:57 -0500

    Huge overhaul of procedural generation
    Fuck that last one. That sucked to make and wouldn't work. here is the rough new one

--- Assets/Scripts/proceduralGeneration.cs
+++ Assets/Scripts/proceduralGeneration.cs
@@ -1,111 +1,84 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class DungeonGenerator : MonoBehaviour
+public class RoomGeneration : MonoBehaviour
 {
-    public GameObject[] roomPrefabs;
-    public GameObject[] enemyPrefabs;
-    public GameObject bossRoomPrefab;
-    public int maxRooms = 10;
-    public int maxEnemiesPerRoom = 3;
-    public float branchProbability = 0.5f;
-    public int maxBranches = 3;
-    public int roomsBeforeBoss = 5;
-    public bool bossHasSpawned = false;
-    public float roomPadding = 1.0f;
-    public bool checkClipping = true;
-    public Vector3 roomScale = new Vector3(10, 10, 10);
-
-    private List<GameObject> rooms = new List<GameObject>();
-    private List<GameObject> enemies = new List<GameObject>();
-    private int roomCount = 0;
-
-    void Start()
+    [SerializeField] public GameObject[] rooms;
+    [SerializeField] public GameObject bossRoom;
+    [SerializeField] public int maxRooms = 10;
+    [SerializeField] public GameObject startingRoom;
+    private GameObject currentRoom;
+    private int roomCount;
+    private bool bossSpawned = false;
+    private List<GameObject> spawnedRooms = new List<GameObject>();
+    public Vector3 roomOffset;
+    private void Start()
     {
-        GenerateDungeon(Vector3.zero);
+        roomCount = 1;
+        currentRoom = Instantiate(startingRoom, transform.position, Quaternion.identity);
+        currentRoom.GetComponent<Room>().OnExit.AddListener(SpawnRoom);
+        currentRoom.GetComponent<Room>().OnExit.AddListener(DestroyRoom);
+        currentRoom.GetComponent<Room>().OnEnter.AddListener(MakeCurrentRoom);
+        spawnedRooms.Add(currentRoom);
     }
 
-    void GenerateDungeon(Vector3 position)
+    private void SpawnRoom()
     {
-        if (roomCount >= maxRooms && bossHasSpawned)
+        if (!bossSpawned)
         {
-            return;
-        }
-
-        GameObject room;
-        Vector3 spawnPos = FindSpawnPosition(position);
-        if (spawnPos == Vector3.negativeInfinity)
-        {
-            return;
-        }
-
-        if (roomCount < roomsBeforeBoss && Random.value > 0.5f)
-        {
-            room = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], spawnPos, Quaternion.identity);
-        }
-        else
-        {
-            room = Instantiate(bossRoomPrefab, spawnPos, Quaternion.identity);
-        }
-        rooms.Add(room);
-        roomCount++;
-        GenerateEnemies(room);
-        GenerateDoors(room);
-    }
-
-    Vector3 FindSpawnPosition(Vector3 position)
-    {
-        int iterations = 0;
-        int maxIterations = 10;
-        while (IsClipping(position) && iterations < maxIterations)
-        {
-            position += new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
-            iterations++;
-        }
-        if (iterations >= maxIterations)
-        {
-            return Vector3.negativeInfinity;
-        }
-        return position;
-    }
-    void GenerateEnemies(GameObject room)
-    {
-        int enemyCount = Random.Range(0, maxEnemiesPerRoom);
-        for (int i = 0; i < enemyCount; i++)
-        {
-            Vector3 enemyPos = new Vector3(Random.Range(-roomScale.x / 2, roomScale.x / 2), 0, Random.Range(-roomScale.z / 2, roomScale.z / 2));
-            GameObject enemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], room.transform.position + enemyPos, Quaternion.identity);
-            enemies.Add(enemy);
-        }
-    }
-
-    void GenerateDoors(GameObject room)
-    {
-        int doorCount = Random.Range(1, maxBranches + 1);
-        for (int i = 0; i < doorCount; i++)
-        {
-            if (Random.value < branchProbability)
+            if (roomCount >= maxRooms)
             {
-                Transform door = room.transform.GetChild(i);
-                Vector3 nextPosition = door.position + door.forward * roomPadding;
-                GenerateDungeon(nextPosition);
+                SpawnBossRoom();
+                bossSpawned = true;
+                return;
+            }
+            else
+            {
+                roomCount++;
+                GameObject[] exits = currentRoom.GetComponent<Room>().exits;
+                for (int i = 0; i < exits.Length; i++)
+                {
+                    GameObject exit = exits[i];
+                    int rand = Random.Range(0, rooms.Length);
+                    GameObject newRoom = Instantiate(rooms[rand], exit.transform.position + roomOffset, Quaternion.identity);
+                    newRoom.GetComponent<Room>().OnExit.AddListener(SpawnRoom);
+                    newRoom.GetComponent<Room>().OnEnter.AddListener(MakeCurrentRoom);
+                    newRoom.GetComponent<Room>().OnEnter.AddListener(DestroyRoom);
+                    Vector3 newEntrance = newRoom.GetComponent<Room>().entrance.transform.position;
+                    Vector3 exitPos = exit.transform.position;
+                    Quaternion newRoomRotation = Quaternion.FromToRotation(newEntrance - exitPos, exitPos - newEntrance);
+                    newRoom.transform.rotation = newRoomRotation;
+                    spawnedRooms.Add(newRoom);
+                }
             }
         }
     }
-
-    bool IsClipping(Vector3 position)
+    private void MakeCurrentRoom()
     {
-        Vector3 extents = roomScale / 2;
-        Collider[] colliders = Physics.OverlapBox(position, extents, Quaternion.identity, ~0);
-        for (int i = 0; i < colliders.Length; i++)
+        currentRoom = this.gameObject;
+    }
+
+    private void SpawnBossRoom()
+    {
+        GameObject[] exits = currentRoom.GetComponent<Room>().exits;
+        for (int i = 0; i < exits.Length; i++)
         {
-            if (colliders[i].gameObject.CompareTag("Room"))
-            {
-                return true;
-            }
+            GameObject exit = exits[i];
+            Vector3 newRoomPosition = exit.transform.position + roomOffset;
+            GameObject newBossRoom = Instantiate(bossRoom, newRoomPosition, Quaternion.identity);
         }
-        return false;
+    }
+    private void DestroyRoom()
+    {
+        //Destroy all previous rooms that haven't been entered
+        foreach (GameObject room in spawnedRooms)
+        {
+            if (room != currentRoom)
+                Destroy(room);
+        }
+        //Clear the list of spawned rooms
+        spawnedRooms.Clear();
+        //Add the current room to the list
+        spawnedRooms.Add(currentRoom);
     }
 }
-
-
