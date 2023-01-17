cbdf681459b2fd0416de1d8a15f0c2fb4c255503
DomBarbuto <DABarbuto@student.fullsail.edu>
2023-01-17 17:03:56 -0500

    Turret setup done

--- Assets/Scripts/enemyTurretHitDetection.cs
+++ Assets/Scripts/enemyTurretHitDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyTurretHitDetection : MonoBehaviour
+public class enemyTurretHitDetection : MonoBehaviour, IDamage
 {
     [SerializeField] enemyTurret brain;
     public void takeDamage(int dmg)
